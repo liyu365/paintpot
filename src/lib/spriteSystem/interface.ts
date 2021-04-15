87676bfe25c14ae86b47badeb020fb9a22002e77
liyu <webcitizen@163.com>
2021-04-15 10:13:36 +0800

    modify

--- src/lib/spriteSystem/interface.ts
+++ src/lib/spriteSystem/interface.ts
@@ -1,70 +1,70 @@
 import { mat2d, vec2, Inset } from "../math2d";
 import { CanvasMouseEvent, CanvasKeyBoardEvent } from "../application";
-import { Rect, Circle, Grid, Ellipse, Line, ConvexPolygon, Scale9Grid, Bone, BezierPath, EndClipShape, EmptyShape } from "./shapes";
+import { Rect, Circle, Grid, Ellipse, Line, ConvexPolygon, Scale9Grid, BezierPath, EndClipShape, EmptyShape } from "./shapes";
 import { Sprite2D } from "./sprite2d";
 
 export enum ERenderType {
-    CUSTOM,
-    STROKE,
-    FILL,
-    STROKE_FILL,
-    CLIP
+  CUSTOM,
+  STROKE,
+  FILL,
+  STROKE_FILL,
+  CLIP
 }
 
 export interface ITransformable {
-    x: number;
-    y: number;
+  x: number;
+  y: number;
 
-    rotation: number;
+  rotation: number;
 
-    scaleX: number;
-    scaleY: number;
+  scaleX: number;
+  scaleY: number;
 
-    getWorldMatrix(): mat2d;
-    getLocalMatrix(): mat2d;
+  getWorldMatrix(): mat2d;
+  getLocalMatrix(): mat2d;
 
 }
 
 export interface IRenderState {
-    isVisible: boolean;
-    showCoordSystem: boolean;
-    lineWidth: number;
-    fillStyle: string | CanvasGradient | CanvasPattern;
-    strokeStyle: string | CanvasGradient | CanvasPattern;
-    renderType: ERenderType;
+  isVisible: boolean;
+  showCoordSystem: boolean;
+  lineWidth: number;
+  fillStyle: string | CanvasGradient | CanvasPattern;
+  strokeStyle: string | CanvasGradient | CanvasPattern;
+  renderType: ERenderType;
 }
 
 
 export interface IHittable {
-    hitTest(localPt: vec2, transform: ITransformable): boolean;
+  hitTest(localPt: vec2, transform: ITransformable): boolean;
 }
 
 export interface IDrawable {
-    beginDraw(transformable: ITransformable, state: IRenderState, context: CanvasRenderingContext2D): void;
-    draw(transformable: ITransformable, state: IRenderState, context: CanvasRenderingContext2D): void;
-    endDraw(transformable: ITransformable, state: IRenderState, context: CanvasRenderingContext2D): void;
+  beginDraw(transformable: ITransformable, state: IRenderState, context: CanvasRenderingContext2D): void;
+  draw(transformable: ITransformable, state: IRenderState, context: CanvasRenderingContext2D): void;
+  endDraw(transformable: ITransformable, state: IRenderState, context: CanvasRenderingContext2D): void;
 }
 
 export interface IShape extends IHittable, IDrawable {
-    readonly type: string;
-    data: any;
+  readonly type: string;
+  data: any;
 }
 
 export interface ISpriteContainer {
-    name: string;
-    addSprite(sprite: ISprite): ISpriteContainer;
-    removeSprite(sprite: ISprite): boolean;
-    removeAll(includeThis: boolean): void;
-    getSpriteIndex(sprite: ISprite): number;
-    getSprite(idx: number): ISprite;
-    getSpriteCount(): number;
-    getParentSprite(): ISprite | undefined;
-    readonly sprite: ISprite | undefined;
+  name: string;
+  addSprite(sprite: ISprite): ISpriteContainer;
+  removeSprite(sprite: ISprite): boolean;
+  removeAll(includeThis: boolean): void;
+  getSpriteIndex(sprite: ISprite): number;
+  getSprite(idx: number): ISprite;
+  getSpriteCount(): number;
+  getParentSprite(): ISprite | undefined;
+  readonly sprite: ISprite | undefined;
 }
 
 export enum EOrder {
-    PREORDER,
-    POSTORDER
+  PREORDER,
+  POSTORDER
 }
 
 export type UpdateEventHandler = ((spr: ISprite, mesc: number, diffSec: number, travelOrder: EOrder) => void);
@@ -73,139 +73,135 @@ export type KeyboardEventHandler = ((spr: ISprite, evt: CanvasKeyBoardEvent) => 
 export type RenderEventHandler = (spr: ISprite, context: CanvasRenderingContext2D, renderOreder: EOrder) => void;
 
 export interface ISprite extends ITransformable, IRenderState {
-    name: string;
-    shape: IShape;
-    owner: ISpriteContainer;
-    data: any;
+  name: string;
+  shape: IShape;
+  owner: ISpriteContainer;
+  data: any;
 
-    hitTest(localPt: vec2): boolean;
-    update(mesc: number, diff: number, order: EOrder): void;
-    draw(context: CanvasRenderingContext2D): void;
+  hitTest(localPt: vec2): boolean;
+  update(mesc: number, diff: number, order: EOrder): void;
+  draw(context: CanvasRenderingContext2D): void;
 
-    mouseEvent: MouseEventHandler | null;
-    keyEvent: KeyboardEventHandler | null;
-    updateEvent: UpdateEventHandler | null;
-    renderEvent: RenderEventHandler | null;
+  mouseEvent: MouseEventHandler | null;
+  keyEvent: KeyboardEventHandler | null;
+  updateEvent: UpdateEventHandler | null;
+  renderEvent: RenderEventHandler | null;
 }
 
 export interface IDispatcher {
-    readonly container: ISpriteContainer;
-    dispatchUpdate(msec: number, diffSec: number): void;
-    dispatchDraw(context: CanvasRenderingContext2D): void;
-    dispatchMouseEvent(evt: CanvasMouseEvent): void;
-    dispatchKeyEvent(evt: CanvasKeyBoardEvent): void;
+  readonly container: ISpriteContainer;
+  dispatchUpdate(msec: number, diffSec: number): void;
+  dispatchDraw(context: CanvasRenderingContext2D): void;
+  dispatchMouseEvent(evt: CanvasMouseEvent): void;
+  dispatchKeyEvent(evt: CanvasKeyBoardEvent): void;
 }
 
 export class SpriteFactory {
 
-    public static createGrid(w: number, h: number, xStep: number = 10, yStep: number = 10): IShape {
-        return new Grid(w, h, xStep, yStep);
+  public static createGrid(w: number, h: number, xStep: number = 10, yStep: number = 10): IShape {
+    return new Grid(w, h, xStep, yStep);
+  }
+
+  public static createCircle(radius: number): IShape {
+    return new Circle(radius);
+  }
+
+  public static createRect(w: number, h: number, u: number = 0, v: number = 0): IShape {
+    return new Rect(w, h, u, v);
+  }
+
+  public static createEllipse(radiusX: number, radiusY: number): IShape {
+    return new Ellipse(radiusX, radiusY);
+  }
+
+  public static createPolygon(points: vec2[]): IShape {
+    if (points.length < 3) {
+      throw new Error("多边形顶点数量必须大于或等于3!!!");
     }
+    return new ConvexPolygon(points);
+  }
 
-    public static createCircle(radius: number): IShape {
-        return new Circle(radius);
-    }
+  public static createScale9Grid(data: Scale9Data, width: number, height: number, u: number = 0, v: number = 0): IShape {
+    return new Scale9Grid(data, width, height, u, v);
+  }
 
-    public static createRect(w: number, h: number, u: number = 0, v: number = 0): IShape {
-        return new Rect(w, h, u, v);
-    }
+  public static createLine(start: vec2, end: vec2): IShape {
+    let line: Line = new Line();
+    line.start = start;
+    line.end = end;
+    return line;
+  }
 
-    public static createEllipse(radiusX: number, radiusY: number): IShape {
-        return new Ellipse(radiusX, radiusY);
-    }
+  public static createXLine(len: number = 10, t: number = 0): IShape {
+    return new Line(len, t);
+  }
 
-    public static createPolygon(points: vec2[]): IShape {
-        if (points.length < 3) {
-            throw new Error("多边形顶点数量必须大于或等于3!!!");
-        }
-        return new ConvexPolygon(points);
-    }
+  public static createBezierPath(points: vec2[], isCubic: boolean = false): IShape {
+    return new BezierPath(points, isCubic);
+  }
 
-    public static createScale9Grid(data: Scale9Data, width: number, height: number, u: number = 0, v: number = 0): IShape {
-        return new Scale9Grid(data, width, height, u, v);
-    }
+  public static endCLipShape: IShape = new EndClipShape();
 
-    public static createLine(start: vec2, end: vec2): IShape {
-        let line: Line = new Line();
-        line.start = start;
-        line.end = end;
-        return line;
-    }
+  public static emptyShape: IShape = new EmptyShape();
 
-    public static createXLine(len: number = 10, t: number = 0): IShape {
-        return new Line(len, t);
-    }
+  public static createClipSprite(): ISprite {
+    let spr: ISprite = new Sprite2D(SpriteFactory.endCLipShape, 'clipSprite');
+    spr.renderType = ERenderType.CLIP;
+    return spr;
+  }
 
-    public static createBone(len: number = 10, t: number = 0): IShape {
-        return new Bone(len, t);
-    }
+  public static createSprite(shape: IShape, name: string = ''): ISprite {
+    let spr: ISprite = new Sprite2D(shape, name);
+    return spr;
+  }
 
-    public static createBezierPath(points: vec2[], isCubic: boolean = false): IShape {
-        return new BezierPath(points, isCubic);
-    }
-
-    public static endCLipShape: IShape = new EndClipShape();
-
-    public static emptyShape: IShape = new EmptyShape();
-
-    public static createClipSprite(): ISprite {
-        let spr: ISprite = new Sprite2D(SpriteFactory.endCLipShape, 'clipSprite');
-        spr.renderType = ERenderType.CLIP;
-        return spr;
-    }
-
-    public static createSprite(shape: IShape, name: string = ''): ISprite {
-        let spr: ISprite = new Sprite2D(shape, name);
-        return spr;
-    }
-
-    public static createISprite(shape: IShape, x: number = 0, y: number = 0, rotation: number = 0, scaleX: number = 1.0, scaleY: number = 1.0, name: string = ' '): ISprite {
-        let spr: ISprite = new Sprite2D(shape, name);
-        spr.x = x;
-        spr.y = y;
-        spr.rotation = rotation;
-        spr.scaleX = scaleX;
-        spr.scaleY = scaleY;
-        return spr;
-    }
+  public static createISprite(shape: IShape, x: number = 0, y: number = 0, rotation: number = 0, scaleX: number = 1.0, scaleY: number = 1.0, name: string = ' '): ISprite {
+    let spr: ISprite = new Sprite2D(shape, name);
+    spr.x = x;
+    spr.y = y;
+    spr.rotation = rotation;
+    spr.scaleX = scaleX;
+    spr.scaleY = scaleY;
+    return spr;
+  }
 }
 
 export enum EImageFillType {
-    NONE,
-    STRETCH,
-    REPEAT,
-    REPEAT_X,
-    REPEAT_Y
+  NONE,
+  STRETCH,
+  REPEAT,
+  REPEAT_X,
+  REPEAT_Y
 }
 
 export class Scale9Data {
-    public image: HTMLImageElement;
-    private _inset: Inset;
+  public image: HTMLImageElement;
+  private _inset: Inset;
 
-    public set inset(value: Inset) {
-        this._inset = value;
-    }
+  public set inset(value: Inset) {
+    this._inset = value;
+  }
 
-    public get leftMargin(): number {
-        return this._inset.leftMargin;
-    }
+  public get leftMargin(): number {
+    return this._inset.leftMargin;
+  }
 
-    public get rightMargin(): number {
-        return this._inset.rightMargin;
-    }
+  public get rightMargin(): number {
+    return this._inset.rightMargin;
+  }
 
-    public get topMargin(): number {
-        return this._inset.topMargin;
-    }
+  public get topMargin(): number {
+    return this._inset.topMargin;
+  }
 
-    public get bottomMargin(): number {
-        return this._inset.bottomMargin;
-    }
+  public get bottomMargin(): number {
+    return this._inset.bottomMargin;
+  }
 
-    public constructor(image: HTMLImageElement, inset: Inset) {
-        this.image = image;
-        this._inset = inset;
-    }
+  public constructor(image: HTMLImageElement, inset: Inset) {
+    this.image = image;
+    this._inset = inset;
+  }
 }
 
 
--- src/main.ts
+++ src/main.ts
@@ -1,123 +1,14 @@
-import { ISprite, SpriteFactory, IShape, ISpriteContainer, EOrder, ITransformable, IRenderState } from "./lib/spriteSystem/interface";
+import { ISprite, SpriteFactory, IShape, EOrder } from "./lib/spriteSystem/interface";
 import { Sprite2DApplication } from "./lib/spriteSystem/sprite2DApplication";
 import { CanvasMouseEvent, EInputEventType } from "./lib/application";
 import { vec2, Math2D } from "./lib/math2d";
-import { Line, BaseShape2D } from "./lib/spriteSystem/shapes";
+import { Line } from "./lib/spriteSystem/shapes";
 import { SpriteNode, SpriteNodeGroup } from './lib/spriteSystem/sprite2dHierarchicalSystem'
 import { Sprite2D } from './lib/spriteSystem/sprite2d'
 
-class CNodeTextShap extends BaseShape2D {
+import { CNodeTextShap } from './shaps/CNodeTextShap'
+import { LinkTextShap } from './shaps/LinkTextShap'
 
-  public constructor(radius = 1) {
-    super();
-  }
-  public hitTest(localPt: vec2, transform: ITransformable): boolean {
-    return false
-  }
-
-  public draw(transformable: ITransformable, state: IRenderState, context: CanvasRenderingContext2D,): void {
-    const spr = transformable as Sprite2D
-    const text = spr.data.text
-
-    context.save()
-    context.font = "20px Arial";
-    context.textBaseline = "middle"
-    context.textAlign = "left"
-    const w = context.measureText(text).width
-    const h = context.measureText('田').width
-    const padding = 10
-    const Xdeviation = - (w + 2 * padding) / 2 // x轴的偏移量
-
-    context.save()
-    context.beginPath();
-    context.fillStyle = "rgba(0, 0, 255, 0.8)"
-    this.drawRoundRect(context, Xdeviation, 0, w + 2 * padding, h + 2 * padding, 6)
-    //context.rect(Xdeviation , 0 ,w + 2 * padding, h + 2 * padding)
-    context.fill()
-    context.restore()
-
-    context.save()
-    context.beginPath();
-    context.fillStyle = "rgba(255, 255, 255, 1)"
-    context.fillText(text, padding + Xdeviation, padding + h / 2 + 1)
-    context.restore()
-
-    context.restore()
-  }
-
-  private drawRoundRect(cxt: CanvasRenderingContext2D, x: number, y: number, width: number, height: number, radius: number) {
-    cxt.beginPath();
-    cxt.arc(x + radius, y + radius, radius, Math.PI, Math.PI * 3 / 2);
-    cxt.lineTo(width - radius + x, y);
-    cxt.arc(width - radius + x, radius + y, radius, Math.PI * 3 / 2, Math.PI * 2);
-    cxt.lineTo(width + x, height + y - radius);
-    cxt.arc(width - radius + x, height - radius + y, radius, 0, Math.PI * 1 / 2);
-    cxt.lineTo(radius + x, height + y);
-    cxt.arc(radius + x, height - radius + y, radius, Math.PI * 1 / 2, Math.PI);
-    cxt.closePath();
-  }
-
-  public get type(): string {
-    return "CNodeTextShap";
-  }
-}
-
-class LnikTextShap extends BaseShape2D {
-
-  public constructor() {
-    super();
-  }
-  public hitTest(localPt: vec2, transform: ITransformable): boolean {
-    return false
-  }
-
-  public draw(transformable: ITransformable, state: IRenderState, context: CanvasRenderingContext2D,): void {
-    const spr = transformable as Sprite2D
-    const text = spr.data.text
-
-    context.save()
-    context.font = "14px Arial";
-    context.textBaseline = "middle"
-    context.textAlign = "left"
-    const w = context.measureText(text).width
-    const h = context.measureText('田').width
-    const padding = 4
-    const Xdeviation = - (w + 2 * padding) / 2 // x轴的偏移量
-    const Ydeviation = - (h + 2 * padding) / 2 // y轴的偏移量
-
-    context.save()
-    context.beginPath();
-    context.fillStyle = "rgba(0, 0, 255, 0.8)"
-    this.drawRoundRect(context, Xdeviation, Ydeviation, w + 2 * padding, h + 2 * padding, 6)
-    //context.rect(Xdeviation , 0 ,w + 2 * padding, h + 2 * padding)
-    context.fill()
-    context.restore()
-
-    context.save()
-    context.beginPath();
-    context.fillStyle = "rgba(255, 255, 255, 1)"
-    context.fillText(text, padding + Xdeviation, padding + h / 2 + Ydeviation + 1)
-    context.restore()
-
-    context.restore()
-  }
-
-  private drawRoundRect(cxt: CanvasRenderingContext2D, x: number, y: number, width: number, height: number, radius: number) {
-    cxt.beginPath();
-    cxt.arc(x + radius, y + radius, radius, Math.PI, Math.PI * 3 / 2);
-    cxt.lineTo(width - radius + x, y);
-    cxt.arc(width - radius + x, radius + y, radius, Math.PI * 3 / 2, Math.PI * 2);
-    cxt.lineTo(width + x, height + y - radius);
-    cxt.arc(width - radius + x, height - radius + y, radius, 0, Math.PI * 1 / 2);
-    cxt.lineTo(radius + x, height + y);
-    cxt.arc(radius + x, height - radius + y, radius, Math.PI * 1 / 2, Math.PI);
-    cxt.closePath();
-  }
-
-  public get type(): string {
-    return "LnikTextShap";
-  }
-}
 
 class topologyApplication {
   private _app: Sprite2DApplication
@@ -219,7 +110,7 @@ class topologyApplication {
     arrow.fillStyle = 'blue'
     linkN.addSprite(arrow);
 
-    const textSpr: ISprite = new Sprite2D(new LnikTextShap(), 'LnikTextSpr')
+    const textSpr: ISprite = new Sprite2D(new LinkTextShap(), 'LinkTextShap')
     textSpr.showCoordSystem = false
     textSpr.x = 0
     textSpr.y = 0;
