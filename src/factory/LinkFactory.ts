a6488cb54b9ee51e58973f603464c81dcb435646
liyu <webcitizen@163.com>
2021-04-22 15:24:19 +0800

    modify

--- src/factory/LinkFactory.ts
+++ src/factory/LinkFactory.ts
@@ -1,74 +1,68 @@
 import { ISprite, SpriteFactory, IShape, EOrder } from "../lib/spriteSystem/interface";
-import { Sprite2DApplication } from "../lib/spriteSystem/sprite2DApplication";
-import { CanvasMouseEvent, EInputEventType } from "../lib/application";
+import { CanvasMouseEvent } from "../lib/application";
 import { vec2, Math2D } from "../lib/math2d";
-import { Line, Rect } from "../lib/spriteSystem/shapes";
+import { Line } from "../lib/spriteSystem/shapes";
 import { SpriteNode, SpriteNodeGroup } from '../lib/spriteSystem/sprite2dHierarchicalSystem'
 import { Sprite2D } from '../lib/spriteSystem/sprite2d'
-
-import { CNodeTextShap } from '../shaps/CNodeTextShap'
 import { LinkTextShap } from '../shaps/LinkTextShap'
 
 export class LinkFactory {
 
   private static _arrowShap: IShape = SpriteFactory.createPolygon([new vec2(5, 0), new vec2(0, 5), new vec2(0, -5)])
-  private static _linkNodes: Array<SpriteNode> = []
   public static _linkGroups: Array<SpriteNodeGroup> = []
   private static _linkCircleGap = 5
   private static _circleRadius = 30
   private static _sameLinkGap = 25
 
-  public static create(node1: ISprite | undefined, node2: ISprite | undefined, name: string): void {
-    const link: ISprite = SpriteFactory.createSprite(SpriteFactory.createXLine());
-    link.strokeStyle = 'green'
-    link.lineWidth = 4
-    link.data = {}
-    link.data.from = node1
-    link.data.to = node2
-    link.x = 0
-    link.y = 0
+  public static create(from: ISprite, to: ISprite, name: string): void {
+    const linkSpr: ISprite = SpriteFactory.createSprite(SpriteFactory.createXLine());
+    linkSpr.strokeStyle = 'green'
+    linkSpr.lineWidth = 4
+    linkSpr.data = {}
+    linkSpr.data.from = from
+    linkSpr.data.to = to
+    linkSpr.x = 0
+    linkSpr.y = 0
+    linkSpr.mouseEvent = this.handleLinkEvent.bind(this)
 
-    link.mouseEvent = this.handleLinkEvent.bind(this)
+    const linkNode = new SpriteNode(linkSpr)
 
-    const linkN = new SpriteNode(link)
+    const arrowSpr: ISprite = SpriteFactory.createSprite(this._arrowShap)
+    arrowSpr.fillStyle = 'blue'
+    linkNode.addSprite(arrowSpr);
 
-    const arrow: ISprite = SpriteFactory.createSprite(this._arrowShap)
-    arrow.fillStyle = 'blue'
-    linkN.addSprite(arrow);
-
-    const textSpr: ISprite = new Sprite2D(new LinkTextShap(), 'LinkTextShap')
+    const textSpr: ISprite = new Sprite2D(new LinkTextShap(), 'linkTextShap')
     textSpr.showCoordSystem = false
     textSpr.x = 0
     textSpr.y = 0;
     textSpr.data = {}
     textSpr.data.text = name
-    linkN.addSprite(textSpr);
+    linkNode.addSprite(textSpr);
 
-    const newGroup = new SpriteNodeGroup({})
-    newGroup.params.from = node1
-    newGroup.params.to = node2
-    const sameGroup: SpriteNodeGroup | null = this.getSameLinkGroup(newGroup)
+
+    const sameGroup: SpriteNodeGroup | null = this.getSameLinkGroup(from, to)
 
     // 如果已经存在相同的group，则放到此group中，否则新建一个group，再作为新group的子集
     if (!sameGroup) {
-      newGroup.addChild(linkN)
-      this._linkNodes.push(linkN)
+      const newGroup = new SpriteNodeGroup({})
+      newGroup.params.from = from
+      newGroup.params.to = to
+      newGroup.addChild(linkNode)
       this._linkGroups.push(newGroup)
       if (newGroup.sprite) {
         newGroup.sprite.updateEvent = this.handleLinkGroupUpdate.bind(this)
       }
     } else {
-      sameGroup.addChild(linkN)
-      this._linkNodes.push(linkN)
+      sameGroup.addChild(linkNode)
     }
   }
 
-  private static getSameLinkGroup(linkGroup: SpriteNodeGroup): SpriteNodeGroup | null {
+  private static getSameLinkGroup(from: ISprite, to: ISprite): SpriteNodeGroup | null {
     let o = null
     this._linkGroups.forEach(item => {
       if (
-        (item.params.from === linkGroup.params.from && item.params.to === linkGroup.params.to) ||
-        (item.params.from === linkGroup.params.to && item.params.to === linkGroup.params.from)
+        (item.params.from === from && item.params.to === to) ||
+        (item.params.from === to && item.params.to === from)
       ) {
         o = item
       }
@@ -87,15 +81,15 @@ export class LinkFactory {
     let to: Sprite2D = linkGroup.params.to
     let pt1: vec2 = new vec2(from.x, from.y)
     let pt2: vec2 = new vec2(to.x, to.y)
-    // const root = this._app.rootContainer as SpriteNode
-    // let rootSpr = root.sprite
+
     let fromParentSpr = from.owner.getParentSprite()
     let toParentSpr = to.owner.getParentSprite()
+    // 把from和to的局部坐标转换为相对于根sprite的局部坐标
     if (fromParentSpr && toParentSpr) {
       pt1 = Math2D.transform(fromParentSpr.getWorldMatrix2(), pt1)
       pt2 = Math2D.transform(toParentSpr.getWorldMatrix2(), pt2)
     }
-    //console.log(pt1, pt2)
+
     const d = Math.sqrt((pt2.y - pt1.y) * (pt2.y - pt1.y) + (pt2.x - pt1.x) * (pt2.x - pt1.x))
     const linkGroupAngle = vec2.getOrientation(pt1, pt2)
     if (linkGroup.sprite) {
@@ -110,7 +104,7 @@ export class LinkFactory {
         if (linkSpr) {
           const gap = this._circleRadius + this._linkCircleGap
           const line: Line = linkSpr.shape as Line
-          line.start = vec2.create(gap, 0);
+          line.start = vec2.create(gap, 0); // 注意line这个shap的start和end的坐标y值都是0
           line.end = vec2.create(d - gap, 0);
           linkSpr.y = this._sameLinkGap * index + - (this._sameLinkGap * (count - 1)) / 2
 
@@ -131,7 +125,7 @@ export class LinkFactory {
             const lnikTextSpr = lnikTextNode.sprite as Sprite2D
             lnikTextSpr.x = d / 2
             lnikTextSpr.y = 0
-            // 此linkSpr定义的方向与包含它的linkGroup的方向相反，所以此linkSpr种的文字也要反向绘制
+            // 此linkSpr定义的方向与包含它的linkGroup的方向相反，所以此linkSpr中的文字也要反向绘制
             if (linkSpr.data.from !== linkGroup.params.from) {
               lnikTextSpr.data.isReverse = true // 为反向显示的linkSpr打标识
               lnikTextSpr.rotation = 180
