732a009c052e6c4f6d4cdf2a294a0699e9399bf4
liyu <webcitizen@163.com>
2021-06-02 15:18:12 +0800

    modify

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
     "@typescript-eslint/no-unused-vars": ["off"],
     "@typescript-eslint/no-inferrable-types": ['off'],
     "@typescript-eslint/no-empty-function": ['off'],
+    "@typescript-eslint/no-var-requires": ['off'],
     "prefer-const": ['off']
   }
 }
