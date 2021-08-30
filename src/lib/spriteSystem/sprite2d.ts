3f7ba095582949d7e63dc2105f69090dadaa734e
liyu <webcitizen@163.com>
2021-08-30 16:51:35 +0800

    整理sprite2d.ts

--- src/lib/spriteSystem/sprite2d.ts
+++ src/lib/spriteSystem/sprite2d.ts
@@ -1,13 +1,23 @@
-import { mat2d, Transform2D, vec2, Math2D } from "../math2d"
-import { ISprite, MouseEventHandler, KeyboardEventHandler, UpdateEventHandler, EOrder, IShape, ERenderType, ITransformable, ISpriteContainer, RenderEventHandler } from "./interface"
+import { mat2d, Transform2D, vec2 } from "../math2d"
+import { ISprite, MouseEventHandler, KeyboardEventHandler, UpdateEventHandler, EOrder, IShape, ERenderType, ISpriteContainer, RenderEventHandler } from "./interface"
 import { TreeNode } from "../treeNode";
 import { SpriteNode } from "./sprite2dHierarchicalSystem";
-import { CanvasMouseEvent, EInputEventType } from "../application";
 
+/**
+ * 表示一个绘制对象，内部存储了：
+ * shape：表示要绘制的路径；
+ * transform：封装了变换矩阵；
+ * fillStyle、strokeStyle、lineWidth：存储了context绘制此对象时需要设置的渲染状态；
+ * renderType：设置最值执行的绘制函数context.fill()或context.stroke()或context.clip()
+ * isXXX：表示当前对象的状态
+ */
 export class Sprite2D implements ISprite {
   public showCoordSystem: boolean = false;
   public renderType: ERenderType = ERenderType.FILL;
   public isVisible: boolean = true;
+  public isSelected: boolean = false
+  public isDragging: boolean = false
+  public isHovering: boolean = false
   public fillStyle: string | CanvasGradient | CanvasPattern = 'white';
   public strokeStyle: string | CanvasGradient | CanvasPattern = 'black';
   public lineWidth: number = 1;
@@ -16,17 +26,15 @@ export class Sprite2D implements ISprite {
 
   public name: string;
   public shape: IShape;
-  public data: any;
-  public owner !: ISpriteContainer;
+  public data: any; // 挂在到此绘制对象的数据
+  public owner !: ISpriteContainer; // 非空断言，因为owner不通过构造函数或内联赋值进行初始化
 
   public mouseEvent: MouseEventHandler | null = null;
   public keyEvent: KeyboardEventHandler | null = null;
-  public updateEvent: UpdateEventHandler | null = null;
-  public renderEvent: RenderEventHandler | null = null;
+  public updateEvent: UpdateEventHandler | null = null; // 被this.update()调用
+  public renderEvent: RenderEventHandler | null = null; // 在this.draw()中被调用两次
 
-  public isSelected: boolean = false
-  public isDragging: boolean = false
-  public isHovering: boolean = false
+  // 缓存鼠标点击到此对象的点，相对于此对象坐标系的局部坐标
   public diffX: number = 0;
   public diffY: number = 0;
 
@@ -152,14 +160,23 @@ export class Sprite2D implements ISprite {
 
   public draw(context: CanvasRenderingContext2D): void {
     if (this.isVisible) {
+
+      // 1.先调用context.restore();
+      // 2.根据this.lineWidth，this.strokeStyle，this.fillStyle设置上下文渲染状态
+      // 3.根据当前对象的世界矩阵，设置context的setTransform()方法
       this.shape.beginDraw(this, this, context);
+
       if (this.renderEvent !== null) {
         this.renderEvent(this, context, EOrder.PREORDER);
       }
+
       this.shape.draw(this, this, context);
+
       if (this.renderEvent !== null) {
         this.renderEvent(this, context, EOrder.POSTORDER);
       }
+
+      // 会调用context.restore();
       this.shape.endDraw(this, this, context);
     }
   }
