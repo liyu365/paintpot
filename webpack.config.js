49cea470c95a64798291c82b8ec44c9a8df49a4d
liyu <webcitizen@163.com>
2021-10-17 23:34:19 +0800

    增加doc

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     main: "./src/main.ts",
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'doc'),
     filename: "[name]-bundle.js",
   },
   resolve: {
